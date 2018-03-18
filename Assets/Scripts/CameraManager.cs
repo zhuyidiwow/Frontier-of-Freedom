9a21bb6e0d2743a9ee3e43837b4062db24cbd5e4
Yidi <zhuyidiwow@gmail.com>
2018-03-17 23:56:42 -0400

    Moving ball

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+	public GameObject FollowedGameObject;
+	public float LerpFactor;
+	
+	private void Update() {
+		Vector3 targetPosition = FollowedGameObject.transform.position;
+		targetPosition.z = transform.position.z;
+//		transform.position = Vector3.Lerp(transform.position, targetPosition, LerpFactor * Time.deltaTime);
+		transform.position = targetPosition;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float Force;
+	
+	private Rigidbody rb;
+	private Coroutine addForceCoroutine;
+	private Plane plane;
+	
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+		plane = new Plane(Vector3.back, transform.position);
+	}
+
+	private void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			float distance;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			plane.Raycast(ray, out distance);
+			Vector3 clickedPos = ray.origin + ray.direction * distance;
+			Vector3 dir = transform.position - clickedPos;
+//			Debug.DrawLine(transform.position, clickedPos, Color.white, 3f);
+			AddForce(dir * Force, 0.1f);
+		}
+		
+		
+	}
+
+	private void AddForce(Vector3 force, float duration) {
+		if (addForceCoroutine != null) StopCoroutine(addForceCoroutine);
+		addForceCoroutine = StartCoroutine(AddForceCoroutine(force, duration));
+	}
+
+	private IEnumerator AddForceCoroutine(Vector3 force, float duration) {
+		float elapsedTime = 0;
+		while (elapsedTime < duration) {
+			rb.AddForce(force, ForceMode.Force);
+			yield return null;
+			elapsedTime += Time.deltaTime;
+		}
+
+		addForceCoroutine = null;
+	}
+}
