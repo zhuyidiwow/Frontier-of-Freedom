eee476d725c53a136f079f3a8e1e677a0aa36259
Yidi-Z <zhuyidiwow@gmail.com>
2018-03-29 13:07:58 -0400

    Playable in class

---
+++ Assets/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager Instance;
+	
+	[SerializeField] private Text powerupText;
+
+	private int power = 10;
+
+	private void Start() {
+		Instance = this;
+		PickupPower();
+	}
+
+	public void PickupPower() {
+		power++;
+		powerupText.text = "Powerup: " + power.ToString();
+	}
+
+	public bool HasPower() {
+		return power >= 1;
+	}
+	public void UsePower() {
+		power--;
+		powerupText.text = "Powerup: " + power.ToString();
+	}
+}
