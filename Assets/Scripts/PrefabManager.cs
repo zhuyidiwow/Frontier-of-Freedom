0fefec9cc842fb7f1700b142f8ee59e410e3740d
Yidi-Z <zhuyidiwow@gmail.com>
2018-04-08 16:25:19 -0400

    Weapon pickable

---
+++ Assets/Scripts/PrefabManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+
+public class PrefabManager : MonoBehaviour {
+    public static PrefabManager Instance;
+
+    public Weapon MissileLauncher;
+    
+    private void Awake() {
+        if (Instance == null) Instance = this;
+    }
+}
---
+++ Assets/Scripts/WeaponPickable.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+
+public class WeaponPickable : Pickable {
+
+    public enum EWeapon {
+        MISSILE_LAUNCHER
+    }
+
+    public EWeapon Type;
+
+    private void Start() {
+        if (Random.Range(0f, 1f) < 0.5f) {
+            Destroy(gameObject);
+        }
+    }
+
+    protected override void OnPickUp() {
+        Weapon weapon;
+        switch (Type) {
+            case EWeapon.MISSILE_LAUNCHER:
+                weapon = PrefabManager.Instance.MissileLauncher;
+                break;
+            default:
+                weapon = PrefabManager.Instance.MissileLauncher;
+                break;
+        }
+        Player.Instance.PickUpWeapon(weapon);
+        Destroy(gameObject);
+    }
+}
