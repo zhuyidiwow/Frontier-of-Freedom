dc7c7efe2f5c929c2854feea7c4bf6919657eb8b
Yidi-Z <zhuyidiwow@gmail.com>
2018-04-08 17:20:14 -0400

    Add subchunks for more randomness

---
+++ Assets/SubChunk.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubChunk : MonoBehaviour {
+
+	public float SurvivalRate;
+
+	private void Start() {
+		if (Random.Range(0f, 1f) >= SurvivalRate) {
+			Destroy(gameObject);
+		}
+	}
+}
