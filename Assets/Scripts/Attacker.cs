eb28a07727ae2097f5f05cdd4c4e1d550d68409f
Yidi-Z <zhuyidiwow@gmail.com>
2018-04-09 17:32:17 -0400

    Enemies now have distinct shapes!

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -13,12 +13,14 @@ public class Attacker : Enemy {
 
     public float ShootInterval;
 
+    [SerializeField] private GameObject weapon;
     [SerializeField] private float bulletSpeed;
     [SerializeField] private float stoppingDistance;
     [SerializeField] private float continueChasingDistance;
     
     private EEnemyMode enemyMode;
     private float lastShotTime;
+    private Vector3 originalWeaponScale;
     
     private void Start() {
         player = Player.Instance;
@@ -26,6 +28,7 @@ public class Attacker : Enemy {
         maxSpeed *= Random.Range(0.5f, 1.5f);
         StartCoroutine(SelfDestroyCheck());
         enemyMode = EEnemyMode.MOVE;
+        originalWeaponScale = weapon.transform.localScale;
     }
 
     private void FixedUpdate() {
@@ -49,16 +52,21 @@ public class Attacker : Enemy {
             default:
                 throw new ArgumentOutOfRangeException();
         }
+        weapon.transform.Rotate(Vector3.forward, 360f * Time.deltaTime, Space.Self);
         
         transform.LookAt(player.transform.position);
     }
 
     private void ShootAtPlayer() {
-        if (Time.time - lastShotTime > ShootInterval) {
+        float elapsedTime = Time.time - lastShotTime;
+        
+        if (elapsedTime > ShootInterval) {
             lastShotTime = Time.time;
             EnemyBullet enemyBullet = Instantiate(PrefabManager.Instance.EnemyBullet, transform.position + transform.forward * 0.5f,
                 Quaternion.identity);
             enemyBullet.Initialize(Damage, bulletSpeed);
+        } else {
+            weapon.transform.localScale = originalWeaponScale * (1f + elapsedTime / ShootInterval);
         }
     }
     
