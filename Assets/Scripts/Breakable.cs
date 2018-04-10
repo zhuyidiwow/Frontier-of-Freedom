a5c460587f74a43a394279f0a1eea09ff9a2589e
Yidi <zhuyidiwow@gmail.com>
2018-04-10 00:11:59 -0400

    Tried adding score to breaking bricks, but it doesn't feel quite right

--- Assets/Scripts/Breakable.cs
+++ Assets/Scripts/Breakable.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Breakable : MonoBehaviour {
-
 	[SerializeField] private GameObject breakParticle;
 
-
 	public virtual void Break() {
+		//TODO: fix this
+//		if (gameObject.name.Contains("Cube")) {
+//			Vector3 pos = transform.position;
+//			Vector3 dir = (pos - Player.Instance.transform.position).normalized;
+//			ScoreText text = Instantiate(PrefabManager.Instance.ScoreText, pos, Quaternion.identity);
+//			text.Initialize(1, dir, pos); 
+//		}
+		
 		GameObject particle = Instantiate(breakParticle, transform.position, Quaternion.identity);
 		Destroy(particle, 3f);
 		Destroy(gameObject);
